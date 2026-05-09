70ba371dd4997e4315d36ecec0f225dc4ec1effa
Kat R. <56617952+KxttyKxt@users.noreply.github.com>
2026-05-08 23:29:52 -0400

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+    private static final Map<Character, Character> parMap = Map.of(
+			')', '(',
+			']', '[',
+			'}', '{'
+	);
+
+    public boolean isValid(String s) {
+        Stack<Character> parStack = new Stack<>();
+
+		for (char c : s.toCharArray()) {
+			if (parMap.containsValue(c))
+				parStack.push(c);
+			else if (parStack.isEmpty() || parStack.pop() != parMap.get(c))
+				return false;
+		}
+
+		return parStack.isEmpty();
+    }
+}
